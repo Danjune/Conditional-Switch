e1429509ef9ee03f0597fa1800821cd9f5bfa24d
Danjune <daniel.ayala621@gmail.com>
2024-05-21 23:15:36 -0600

    Switch inicial (siempre es una solo resultado)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Random random = new Random();
+
+        int dia = 5;
+        String nombreDia = null;
+
+        switch (dia){
+            case 1:
+                nombreDia = "Lunes";
+            break;
+
+            case 2:
+                nombreDia = "Martes";
+            break;
+
+            case 3:
+                nombreDia = "Miercoles";
+            break;
+
+            case 4:
+                nombreDia = "Jueves";
+            break;
+
+            case 5:
+                nombreDia = "Viernes";
+            break;
+
+            case 6:
+                nombreDia = "Sabado";
+            break;
+
+            case 7:
+                nombreDia = "Domingo";
+            break;
+
+            default:
+                nombreDia = "No existe ese dia";
+            break;
         }
+        System.out.println("El dia es: " + nombreDia);
+
     }
 }
